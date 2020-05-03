31ca824e3de0ddf63647cc22b5eafb45a404ec5d
Joshua Williams <j.o-williams@outlook.com>
2020-05-03 02:41:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,39 +1,41 @@
+require 'pry'
 class Artist
 
-  attr_accessor :name, :songs
-  @@all =[]
+    @@all = []
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+    attr_accessor :name
 
-  def add_song(song)
-    @songs << song
-  end
-
-  def save
-    @@all << self #saves the artist name to the all array
-  end
-
-  def self.all
-    @@all #displays the all array
-  end
-
-  def self.find_or_create_by_name(artist_name)
-    found_artist = self.all.find {|artist| artist.name == artist_name}
-    if found_artist
-      found_artist
-    else
-      new_artist = self.new(artist_name)
-      new_artist.save
-      new_artist
+    def initialize(name)
+        @name = name
+        @@all << self
+        @songs = []
     end
-  end
 
-  def print_songs
-    @songs.each do |song|
-      puts song.name
+    def self.all
+        @@all
     end
-  end
-end
+
+    def add_song(song)
+        @songs << song
+    end
+
+    def songs
+        Song.all.each do |song|
+            if song.artist == self
+                @songs << song
+            end
+        end
+        @songs
+    end
+
+    def self.find_or_create_by_name(name)
+        self.all.detect {|artist| artist.name == name} || self.new(name)
+    end
+
+    def print_songs
+        @songs.each do |song|
+            puts song.name 
+        end    
+    end
+
+end 
